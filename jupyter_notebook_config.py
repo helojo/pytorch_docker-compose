f41957abe88abe87844450058714a71815e2a0d4
xuyong <93749937@qq.com>
2022-04-18 10:41:28 +0800

    cmt

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -3,3 +3,5 @@ c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.open_browser = False
 c.NotebookApp.port = 8888
 c.NotebookApp.token = ''
+c.NotebookApp.password = 'sha1:4c9cb84cd8ed:71aca80f76f8165406bc1bea2b9bad039db5bf20'
+#c.NotebookApp.password = 'sha1:5bc5f07f35ca:ff13778365003b46bcbb71f80835f1c8ac875fdd'
